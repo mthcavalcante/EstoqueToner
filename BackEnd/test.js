b738d21885344ea68791da465c79ea15662af9a0
Matheus Cavalcante <mc2798353@gmail.com>
2024-11-29 20:11:44 -0300

    Fix: Removendo test.js, arquivo desnecessario

--- BackEnd/test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const generatePassword = async () => {
-    const hashedPassword = bcrypt.hashSync('Vitoria0612.', 10);
-    console.log('Senha criptografada:', hashedPassword);
-};
-
-generatePassword();
